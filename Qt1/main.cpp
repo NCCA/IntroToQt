4579454ba1494ea91585e05313a71e2050de9d27
jmacey <jmacey@bournemouth.ac.uk>
2017-11-09 13:24:53 +0000

    reverted

--- Qt1/main.cpp
+++ Qt1/main.cpp
@@ -1,16 +1,16 @@
 #include <QApplication>
 #include <QLabel>
 #include <QPushButton>
-#include <QCalendarWidget>
+#include <QLabel>
 
 
 int main(int argc, char *argv[])
 {
   QApplication app(argc, argv);
-  QCalendarWidget *label = new QCalendarWidget();
+  QLabel *label = new QLabel();
   label->setObjectName(QString::fromUtf8("label"));
   label->setGeometry(QRect(100, 100, 200, 100));
- // label->setText("Hello World");
+  label->setText("Hello World");
   label->show();
   return app.exec();
 }
