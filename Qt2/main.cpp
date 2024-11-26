4f40a2ba0eee721d3b58cdea276d6ab714844d4c
jmacey <jmacey@bournemouth.ac.uk>
2024-11-26 13:23:12 +0000

    fixed Qt2 demo

--- Qt2/main.cpp
+++ Qt2/main.cpp
@@ -12,26 +12,15 @@ int main(int argc, char *argv[])
   // create a main window widget
   QMainWindow mainwin;
   // set the name
-<<<<<<< HEAD
   mainwin.setObjectName(QString::fromUtf8("MainWindow"));
-=======
-  mainwin.setObjectName(QString::fromUtf8("MainWindowTest"));
-  std::cout<<mainwin.objectName().toStdString()<<'\n';
->>>>>>> 4269b9d436f770575c045e26185c3f8a18eb3dcb
   // set the size
   mainwin.resize(200,200);
   // set the title of the window
   mainwin.setWindowTitle("A MainWindow App");
   // create a central widget with the main window as the parent
   QWidget *centralwidget = new QWidget(&mainwin);
-<<<<<<< HEAD
-=======
-  QGridLayout *grid = new QGridLayout(centralwidget);
-  centralwidget->setLayout(grid);
->>>>>>> 4269b9d436f770575c045e26185c3f8a18eb3dcb
   // create a push button with the central widget as the parent
   QPushButton *button = new QPushButton(centralwidget);
-  grid->addWidget(button);
   // set the name
   button->setObjectName(QString::fromUtf8("button"));
   // set the geometry
